4b79d5be4004d444cb0663e6a65205f815ceab35
XOR-IND\chouhan_r <vickrantchouhan@yahoo.in>
2017-09-07 17:29:09 +0530

    oauth

---
+++ OAuthentication1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuthentication1.Startup))]
+namespace OAuthentication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
